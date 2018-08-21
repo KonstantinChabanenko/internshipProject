c09f4d5059d25d1e2b8e95214c8b0840d36d4f5b
KonstantinChabanenko <42552016+KonstantinChabanenko@users.noreply.github.com>
2018-08-21 23:37:16 +0300

    Project update
    Now header is adaptive. I fixed bugs with search-box and navigation-menu items. At the moment wishlist doesn't layer on the navigation-menu.

--- app/js/main-js.js
+++ app/js/main-js.js
@@ -1,16 +1,16 @@
 $('document').ready(function(){
-  $('.drop-down-search').hide();
-  $('.search-box').on('click', function(){
-    $('.drop-down-search').slideDown(1000);
+  $('#drop-down-search').hide();
+  $('#search-box').on('click', function(){
+    $('#drop-down-search').slideDown(1000);
   });
-  $('.search-box').click(function(e) {
+  $('#search-box').click(function(e) {
     e.stopPropagation();
   });
-  $('.drop-down-search').click(function(e) {
+  $('#drop-down-search').click(function(e) {
     e.stopPropagation();
   });
   $('body').click(function(){
-    $('.drop-down-search').hide();
-    $('.search-box').val('');
+    $('#drop-down-search').hide();
+    $('#search-box').val('');
   });
 });
